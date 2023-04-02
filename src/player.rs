38a0b420ff8bb3b6ef24150ddc37621ecddb7071
Askor <askor@users.noreply.github.com>
2023-04-02 15:12:00 +0200

    FlyCam modified

--- src/player.rs
+++ src/player.rs
@@ -1,47 +1,194 @@
-use crate::actions::Actions;
-use crate::GameState;
 use bevy::prelude::*;
+use bevy::ecs::event::{Events, ManualEventReader};
+use bevy::input::mouse::MouseMotion;
+use bevy::window::{CursorGrabMode, PrimaryWindow};
+use crate::GameState;
 
 pub struct PlayerPlugin;
 
-const SPEED: f32 = 10.;
+impl Plugin for PlayerPlugin {
+    fn build(&self, app: &mut App) {
+        app
+        .add_system(setup.in_schedule(OnEnter(GameState::Playing)))
+        .add_plugin(NoCameraPlayerPlugin)
+        ;
+    }
+}
+
 
 #[derive(Component)]
 pub struct Player;
 
-/// This plugin handles player related stuff like movement
-/// Player logic is only active during the State `GameState::Playing`
-impl Plugin for PlayerPlugin {
-    fn build(&self, app: &mut App) {
-        app.add_system(spawn_player.in_schedule(OnEnter(GameState::Playing)))
-            .add_system(move_player.in_set(OnUpdate(GameState::Playing)));
-    }
-}
-
-fn spawn_player(
+fn setup(
     mut commands: Commands,
 ) {
-    commands
-        .spawn((
-            Camera3dBundle::default(),
-            Player,
-        ));
+    commands.spawn((
+        Camera3dBundle {
+            transform: Transform::from_xyz(0., 2., 24.).looking_at(Vec3::new(0., 1., 0.), Vec3::Y),
+            ..default()
+        },
+        Name::new("Camera"),
+        FlyCam,
+        Player,
+    ));
 }
 
-fn move_player(
-    time: Res<Time>,
-    actions: Res<Actions>,
-    mut player_query: Query<&mut Transform, With<Player>>,
-) {
-    if actions.player_movement.is_none() {
-        return;
+/// Keeps track of mouse motion events, pitch, and yaw
+#[derive(Resource, Default)]
+struct InputState {
+    reader_motion: ManualEventReader<MouseMotion>,
+    pitch: f32,
+    yaw: f32,
+}
+/// Mouse sensitivity and movement speed
+#[derive(Resource)]
+pub struct MovementSettings {
+    pub sensitivity: f32,
+    pub speed: f32,
+}
+
+impl Default for MovementSettings {
+    fn default() -> Self {
+        Self {
+            sensitivity: 0.00012,
+            speed: 12.,
+        }
     }
-    let movement = Vec3::new(
-        actions.player_movement.unwrap().x * SPEED * time.delta_seconds(),
-        actions.player_movement.unwrap().y * SPEED * time.delta_seconds(),
-        0.,
-    );
-    for mut player_transform in &mut player_query {
-        player_transform.translation += movement;
+}
+
+/// A marker component used in queries when you want flycams and not other cameras
+#[derive(Component)]
+pub struct FlyCam;
+
+/// Grabs/ungrabs mouse cursor
+fn toggle_grab_cursor(
+    // mut primary_query: Query<&mut Window, With<PrimaryWindow>>,
+    mut window: &mut Window,
+) {
+    match window.cursor.grab_mode {
+        CursorGrabMode::None => {
+            window.cursor.grab_mode = CursorGrabMode::Confined;
+            window.cursor.visible = false;
+        }
+        _ => {
+            window.cursor.grab_mode = CursorGrabMode::None;
+            window.cursor.visible = true;
+        }
+    }
+}
+
+/// Handles keyboard input and movement
+fn player_move(
+    keys: Res<Input<KeyCode>>,
+    time: Res<Time>,
+    primary_query: Query<&Window, With<PrimaryWindow>>,
+    settings: Res<MovementSettings>,
+    mut query: Query<&mut Transform, With<FlyCam>>,
+) {
+    let Ok(window) = primary_query.get_single() else {
+        warn!("Primary window not found for `player_move`!");
+        return;
+    };
+    
+    for mut transform in query.iter_mut() {
+        let mut velocity = Vec3::ZERO;
+        let local_z = transform.local_z();
+        let forward = -Vec3::new(local_z.x, 0., local_z.z);
+        let right = Vec3::new(local_z.z, 0., -local_z.x);
+
+        for key in keys.get_pressed() {
+            match window.cursor.grab_mode {
+                CursorGrabMode::None => (),
+                _ => match key {
+                    KeyCode::W => velocity += forward,
+                    KeyCode::S => velocity -= forward,
+                    KeyCode::A => velocity -= right,
+                    KeyCode::D => velocity += right,
+                    KeyCode::Space => velocity += Vec3::Y,
+                    KeyCode::LShift => velocity -= Vec3::Y,
+                    _ => (),
+                },
+            }
+        }
+
+        velocity = velocity.normalize_or_zero();
+
+        transform.translation += velocity * time.delta_seconds() * settings.speed
+    }
+}
+
+/// Handles looking around if cursor is locked
+fn player_look(
+    settings: Res<MovementSettings>,
+    mut state: ResMut<InputState>,
+    motion: Res<Events<MouseMotion>>,
+    mut query: Query<&mut Transform, With<FlyCam>>,
+    primary_query: Query<&Window, With<PrimaryWindow>>,
+) {
+    let Ok(window) = primary_query.get_single() else {
+        warn!("Primary window not found for `player_look`!");
+        return;
+    };
+    let mut delta_state = state.as_mut();
+    for mut transform in query.iter_mut() {
+        for ev in delta_state.reader_motion.iter(&motion) {
+            match window.cursor.grab_mode {
+                CursorGrabMode::None => (),
+                _ => {
+                    // Using smallest of height or width ensures equal vertical and horizontal sensitivity
+                    let window_scale = window.height().min(window.width());
+                    delta_state.pitch -=
+                        (settings.sensitivity * ev.delta.y * window_scale).to_radians();
+                    delta_state.yaw -=
+                        (settings.sensitivity * ev.delta.x * window_scale).to_radians();
+                }
+            }
+
+            delta_state.pitch = delta_state.pitch.clamp(-1.54, 1.54);
+
+            // Order is important to prevent unintended roll
+            transform.rotation = Quat::from_axis_angle(Vec3::Y, delta_state.yaw)
+                * Quat::from_axis_angle(Vec3::X, delta_state.pitch);
+        }
+    }
+}
+
+fn cursor_grab(
+    keys: Res<Input<KeyCode>>,
+    mut primary_query: Query<&mut Window, With<PrimaryWindow>>,
+) {
+    let Ok(mut window) = primary_query.get_single_mut() else {
+        warn!("Primary window not found for `cursor_grab`!");
+        return;
+    };
+    if keys.just_pressed(KeyCode::Escape) {
+        toggle_grab_cursor(&mut window);
+    }
+}
+
+// Grab cursor when an entity with FlyCam is added
+fn initial_grab_on_flycam_spawn(
+    query_added: Query<Entity, Added<FlyCam>>,
+    mut primary_query: Query<&mut Window, With<PrimaryWindow>>,
+) {
+    let Ok(mut window) = primary_query.get_single_mut() else {
+        warn!("Primary window not found for `initial_grab_cursor`!");
+        return;
+    };
+    if query_added.is_empty() { return; }
+    
+    toggle_grab_cursor(&mut window);
+}
+
+/// Same as [`PlayerPlugin`] but does not spawn a camera
+pub struct NoCameraPlayerPlugin;
+impl Plugin for NoCameraPlayerPlugin {
+    fn build(&self, app: &mut App) {
+        app.init_resource::<InputState>()
+            .init_resource::<MovementSettings>()
+            .add_system(initial_grab_on_flycam_spawn)
+            .add_system(player_move)
+            .add_system(player_look)
+            .add_system(cursor_grab);
     }
 }
