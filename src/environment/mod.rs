1970ce8c0c75821fe535e229a61496c7241d0ebe
Askor <askor@users.noreply.github.com>
2023-04-02 15:11:33 +0200

    Rapier physics

--- src/environment/mod.rs
+++ src/environment/mod.rs
@@ -1,13 +1,17 @@
-use bevy::prelude::*;
-
-use self::test_env::TestEnvironmentPlugin;
-
 mod test_env;
+mod physics;
+
+use bevy::prelude::*;
+use test_env::TestEnvironmentPlugin;
+
+use self::physics::PhysicsPlugin;
+
 pub struct EnvironmentPlugin;
 
 impl Plugin for EnvironmentPlugin {
     fn build(&self, app: &mut App) {
         app
-            .add_plugin(TestEnvironmentPlugin);
+            .add_plugin(TestEnvironmentPlugin)
+            .add_plugin(PhysicsPlugin);
     }
 }
---
+++ src/environment/physics.rs
@@ -0,0 +1,11 @@
+use bevy::prelude::*;
+use bevy_rapier3d::prelude::*;
+
+pub struct PhysicsPlugin;
+
+impl Plugin for PhysicsPlugin {
+    fn build(&self, app: &mut App) {
+        app
+            .add_plugin(RapierPhysicsPlugin::<NoUserData>::default());
+    }
+}
