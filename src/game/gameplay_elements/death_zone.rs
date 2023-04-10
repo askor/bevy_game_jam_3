b92b604cf6e6dfd78cbeb0edd6de97a02c3229df
Askor <askor@users.noreply.github.com>
2023-04-10 17:17:35 +0200

    Death zone kills you

--- src/game/gameplay_elements/death_zone.rs
+++ src/game/gameplay_elements/death_zone.rs
@@ -1,14 +1,18 @@
-use super::create_physical_box;
+use super::{create_physical_box, ball::GolfBall};
 use bevy::prelude::*;
 use bevy_rapier3d::prelude::*;
 
+pub struct DeathZonePlugin;
+
+impl Plugin for DeathZonePlugin {
+    fn build(&self, app: &mut App) {
+        app.add_system(death_zone_collision);
+    }
+}
+
 #[derive(Component, Reflect, Default)]
 #[reflect(Component)]
-pub(crate) struct DeathZone {
-    pub(crate) x: f32,
-    pub(crate) y: f32,
-    pub(crate) z: f32,
-}
+pub(crate) struct DeathZone;
 
 // On death zone added
 pub(crate) fn add_death_zone(
@@ -31,7 +35,10 @@ pub(crate) fn add_death_zone(
         },
         ground_dims.0, // Collider
         RigidBody::Fixed,
-        Restitution::new(1.0)
+        Restitution::new(1.0),
+        ActiveEvents::COLLISION_EVENTS,
+        Name::new("Death Zone"),
+        DeathZone,
     ));
 }
 
@@ -41,5 +48,27 @@ pub(crate) fn cleanup_death_zone(
 ) {
     for entity in query.iter() {
         commands.entity(entity).despawn_recursive();
+    }
+}
+
+fn death_zone_collision(
+    mut commands: Commands,
+    mut collisions: EventReader<CollisionEvent>,
+    q_death_zone: Query<Entity, With<DeathZone>>,
+    q_ball: Query<Entity, With<GolfBall>>,
+) {
+    for collision in collisions.iter() {
+        info!("Death collisioon!");
+        
+        match collision {
+            CollisionEvent::Started(a, b, _) => {
+                if q_death_zone.get(*a).is_ok() || q_death_zone.get(*b).is_ok() {
+                    info!("Dead!");
+                    let ball = q_ball.single();
+                    commands.entity(ball).despawn_recursive();
+                }
+            },
+            CollisionEvent::Stopped(_, _, _) => (),
+        }
     }
 }
